55e42801bdf32d09b6a9c6abe744490af5a95b06
matt <matt@mmatt.net>
2021-10-08 23:39:18 -0500

    feat(ui): Added base ui
    - Not ready, don't use it yet.

--- ui.py
+++ ui.py
@@ -4,85 +4,9 @@ from datetime import datetime
 import httpx
 import time
 import re
-<<<<<<< HEAD
 from playsound import playsound
 import urllib.request
 from dotenv import load_dotenv
-
-load_dotenv()
-
-def test_tts(self):
-	message = text_field.get()
-	message = re.sub("(?i)cheer\d*", "", message)
-	if message[0] == " ":
-		message = message[1:]
-
-	print(message.split(": "))
-
-	voice = message.split(": ")[0]
-	voice = voice.lower()
-	print(voice)
-	text = message.split(": ")[1]
-	print(text)
-
-	response = httpx.post(
-		"https://api.uberduck.ai/speak",
-		auth=(
-			os.environ.get("UBERDUCK_USERNAME"),
-			os.environ.get("UBERDUCK_SECRET"),
-		),
-		json={
-			"speech": text,
-			"voice": voice,
-		},
-	)
-
-	print(response.json())
-
-	if response.json()["uuid"] is not None:
-		print("UUID recieved. Waiting for TTS to process")
-
-		checkCount = 0
-		waitingToProcess = True
-		while waitingToProcess:
-			checkCount += 1
-
-			ud_ai = httpx.get(
-				f"https://api.uberduck.ai/speak-status?uuid={response.json()['uuid']}",
-				auth=(
-					os.environ.get("UBERDUCK_USERNAME"),
-					os.environ.get("UBERDUCK_SECRET"),
-				),
-			)
-
-			print(ud_ai.json())
-			if ud_ai.json()["path"] != None:
-				print(f"TTS processed after {checkCount} checks")
-				date_string = datetime.now().strftime("%d%m%Y%H%M%S")
-				urllib.request.urlretrieve(
-					ud_ai.json()["path"], f"AI_voice_{date_string}.wav"
-				)
-				time.sleep(1)
-				playsound(f"./AI_voice_{date_string}.wav")
-				os.remove(f"./AI_voice_{date_string}.wav")
-				waitingToProcess = False
-			else:
-				if checkCount > 100:
-					print(
-						f"Failed to recieve a processed TTS after {checkCount} checks. Giving up."
-					)
-					waitingToProcess = False
-				else:
-					print(
-						f"Waiting for TTS to finish processing. {checkCount} checks"
-					)
-					time.sleep(1)
-
-=======
-import urllib.request
-from dotenv import load_dotenv
-import winsound
-
 
 load_dotenv()
 
@@ -139,24 +63,18 @@ def test_tts(self):
                     ud_ai.json()["path"], f"AI_voice_{date_string}.wav"
                 )
                 time.sleep(1)
-                winsound.PlaySound(f"./AI_voice_{date_string}.wav", winsound.SND_ASYNC)
-                time.sleep(1)
+                playsound(f"./AI_voice_{date_string}.wav")
                 os.remove(f"./AI_voice_{date_string}.wav")
                 waitingToProcess = False
-
-            elif ud_ai.json()["failed_at"] != None:
-                print("TTS request failed.")
-                waitingToProcess = False
-
-            elif checkCount > 100:
-                print(
-                    f"Failed to recieve a processed TTS after {checkCount} checks. Giving up."
-                )
-                waitingToProcess = False
             else:
-                print(f"Waiting for TTS to finish processing. {checkCount} checks")
-                time.sleep(1)
->>>>>>> 0853aa4d62d0c6eb2ff374b3fb451904e7230fcb
+                if checkCount > 100:
+                    print(
+                        f"Failed to recieve a processed TTS after {checkCount} checks. Giving up."
+                    )
+                    waitingToProcess = False
+                else:
+                    print(f"Waiting for TTS to finish processing. {checkCount} checks")
+                    time.sleep(1)
 
 
 ui = tk.Tk()
@@ -165,27 +83,6 @@ yo = tk.Label(text="AI tts :D")
 
 yo.pack()
 
-<<<<<<< HEAD
-button = tk.Button(
-	text="Send Test TTS",
-	width=15,
-	height=5,
-	bg="black",
-	fg="white"
-)
-
-button.bind("<Button-1>",  test_tts)
-
-button.pack()
-
-text_field = tk.Entry(
-	width=50
-)
-
-text_field.pack()
-
-ui.mainloop()
-=======
 button = tk.Button(text="Send Test TTS", width=15, height=5, bg="black", fg="white")
 
 button.bind("<Button-1>", test_tts)
@@ -197,4 +94,3 @@ text_field = tk.Entry(width=50)
 text_field.pack()
 
 ui.mainloop()
->>>>>>> 0853aa4d62d0c6eb2ff374b3fb451904e7230fcb
