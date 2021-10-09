e8b7883b9675af68b06f8741b6a18e32088e6aea
matt <30363562+mmattbtw@users.noreply.github.com>
2021-10-08 23:35:22 -0500

    feat(core): Added channel points as an option. (#26)

--- bot.py
+++ bot.py
@@ -39,7 +39,90 @@ log = logging.getLogger()
 logging.basicConfig(level=log_level, format="%(name)s - %(message)s", datefmt="%X")
 
 
-def callback_whisper(uuid: UUID, data: dict) -> None:
+def callback_channel_points(uuid: UUID, data: dict) -> None:
+    print(data)
+
+    if (
+        data["data"]["redemption"]["reward"]["title"].lower()
+        == config["channel_points_reward"].lower()
+    ):
+        message = data["data"]["redemption"]["user_input"]
+        if message[0] == " ":
+            message = message[1:]
+
+        print(message)
+
+        voice = message.split(": ")[0]
+        voice = voice.lower()
+        print(voice)
+        text = message.split(": ")[1]
+        print(text)
+
+        if len(text) > config["MAX_MSG_LENGTH"]:
+            print("Cheered message is longer than the maximum message length")
+            return
+
+        else:
+            response = httpx.post(
+                "https://api.uberduck.ai/speak",
+                auth=(
+                    os.environ.get("UBERDUCK_USERNAME"),
+                    os.environ.get("UBERDUCK_SECRET"),
+                ),
+                json={
+                    "speech": text,
+                    "voice": voice,
+                },
+            )
+
+            print(response.json())
+
+        if response.json()["uuid"] is not None:
+            print("UUID recieved. Waiting for TTS to process")
+
+            checkCount = 0
+            waitingToProcess = True
+            while waitingToProcess:
+                checkCount += 1
+
+                ud_ai = httpx.get(
+                    f"https://api.uberduck.ai/speak-status?uuid={response.json()['uuid']}",
+                    auth=(
+                        os.environ.get("UBERDUCK_USERNAME"),
+                        os.environ.get("UBERDUCK_SECRET"),
+                    ),
+                )
+
+                print(ud_ai.json())
+                if ud_ai.json()["path"] != None:
+                    print(f"TTS processed after {checkCount} checks")
+                    date_string = datetime.now().strftime("%d%m%Y%H%M%S")
+                    urllib.request.urlretrieve(
+                        ud_ai.json()["path"], f"AI_voice_{date_string}.wav"
+                    )
+                    time.sleep(1)
+                    winsound.PlaySound(
+                        f"./AI_voice_{date_string}.wav", winsound.SND_ASYNC
+                    )
+                    time.sleep(1)
+                    os.remove(f"./AI_voice_{date_string}.wav")
+                    waitingToProcess = False
+                elif ud_ai.json()["failed_at"] != None:
+                    print("This TTS request failed.")
+                    waitingToProcess = False
+                elif checkCount > 100:
+                    print(
+                        f"Failed to recieve a processed TTS after {checkCount} checks. Giving up."
+                    )
+                    waitingToProcess = False
+                else:
+                    print(
+                        f"Waiting for TTS to finish processing. {checkCount}/100 checks"
+                    )
+                    time.sleep(1)
+
+
+def callback_bits(uuid: UUID, data: dict) -> None:
     print(data)
 
     bits = data["data"]["bits_used"]
@@ -141,5 +224,12 @@ user_id = twitch.get_users(logins=[os.environ.get("TWITCH_USERNAME")])["data"][0
 pubsub = PubSub(twitch)
 pubsub.start()
 # you can either start listening before or after you started pubsub.
-uuid = pubsub.listen_bits(user_id, callback_whisper)
+if config["BITS_OR_CHANNEL_POINTS"] == "channel_points":
+    uuid = pubsub.listen_channel_points(user_id, callback_channel_points)
+elif (
+    config["BITS_OR_CHANNEL_POINTS"] == "bits"
+    or config["BITS_OR_CHANNEL_POINTS"] is None
+):
+    uuid = pubsub.listen_bits(user_id, callback_bits)
+
 print("Pubsub Ready!")
