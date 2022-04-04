24e3270d9a990e59df1be1b00f08263236794eab
mmattbtw <matt@mmatt.net>
2022-04-04 16:38:02 -0500

    feat(scripts): added messages to python script

--- scripts/install-python.bat
+++ scripts/install-python.bat
@@ -4,13 +4,15 @@
 rem --Download python installer
 curl "https://www.python.org/ftp/python/3.9.12/python-3.9.12-amd64.exe" -o python-installer.exe
 
+@echo installing python!
 rem --Install python
 python-installer.exe /quiet InstallAllUsers=1 PrependPath=1
 
 rem --Refresh Environmental Variables
 call RefreshEnv.cmd
 
+@echo installing dependencies!
 rem --Use python, pip
-pip install -r requirements.txt
+pip install -r ../requirements.txt
 
 pause
