bdb7ae9d821e151068bbd5db7f1362b8d81de674
mmattbtw <matt@mmatt.net>
2022-06-11 00:37:24 -0500

    fix(core): forgot checkCount in a function call

--- bot.py
+++ bot.py
@@ -48,7 +48,7 @@ from twitchAPI.types import AuthScope
 from API.fakeyou import Fakeyou
 from API.uberduck import Uberduck
 
-VERSION: str = "3.1.3"
+VERSION: str = "3.1.4"
 
 JS_STRING: str = """<meta http-equiv="refresh" content="1">"""
 CHEER_REGEX: str = r"(?i)(cheer(?:whal)?|doodlecheer|biblethump|corgo|uni|showlove|party|seemsgood|pride|kappa|frankerz|heyguys|dansgame|elegiggle|trihard|kreygasm|4head|swiftrage|notlikethis|vohiyo|pjsalt|mrdestructoid|bday|ripcheer|shamrock|streamlabs|bitboss|muxy|anon)\d*"
@@ -452,7 +452,7 @@ def request_tts(message: str, failed: Optional[bool] = False) -> None:
                         f"Failed to recieve a processed TTS after {checkCount} checks. Giving up."
                     )
                     waitingToProcess = False
-                    too_many_tries_overlay()
+                    too_many_tries_overlay(checkCount)
 
                     time.sleep(5)
 
