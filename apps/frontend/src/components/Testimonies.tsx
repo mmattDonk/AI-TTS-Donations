791b83761f1fef7000d4d01c1726ce36936cc143
mmattbtw <matt@mmatt.net>
2022-10-06 20:09:21 -0500

    added stars + links + followers to testimonies + real testimonies

--- apps/frontend/src/components/Testimonies.tsx
+++ apps/frontend/src/components/Testimonies.tsx
@@ -1,5 +1,8 @@
 import { Carousel } from "@mantine/carousel";
 import { Avatar, createStyles, Group, Text } from "@mantine/core";
+import Autoplay from "embla-carousel-autoplay";
+import { useRef } from "react";
+import { Star } from "tabler-icons-react";
 
 const useStyles = createStyles((theme) => ({
   body: {
@@ -11,31 +14,52 @@ const useStyles = createStyles((theme) => ({
 const testimonies: CommentSimpleProps[] = [
   {
     author: {
-      name: "mmattbtw",
+      name: "LiptonGod",
       image:
-        "https://static-cdn.jtvnw.net/jtv_user_pictures/aef1b6dd-d45f-4e68-92e5-8c9dfac4122c-profile_image-70x70.png",
+        "https://cdn.7tv.app/pp/60532eaab4d31e459f727bcd/4d77d7a5f1824289a3c67188046d02d3",
+      followers: "923",
     },
-    body: "lorem ipsum dolor sit amet",
+    body: "Ever since seeing AI TTS Donations, I immediately got hooked, You cannot go wrong with having a free AI TTS, most definitely recommend this to those who are not able to pay for stuff like this.",
+    stars: 5,
+  },
+  {
+    author: {
+      name: "AuroR6S",
+      image:
+        "https://static-cdn.jtvnw.net/jtv_user_pictures/73cca255-1a58-40a8-8cb9-983aa9392372-profile_image-300x300.png",
+      followers: "146",
+    },
+    body: "I love this app! It breaks sometimes but its very good!",
+    stars: 5,
   },
 ];
 
 interface CommentSimpleProps {
   body: string;
+  stars: number;
   author: {
     name: string;
     image: string;
+    followers: string;
   };
 }
 
-function Testimonial({ body, author }: CommentSimpleProps) {
+function Testimonial({ body, author, stars }: CommentSimpleProps) {
   const { classes } = useStyles();
+
   return (
     <div>
       <Group>
         <Avatar src={author.image} alt={author.name} radius="xl" />
         <div>
-          <Text size="lg">{author.name}</Text>
+          <Text size="lg">
+            <a href={`https://twitch.tv/${author.name}`}>{author.name}</a>
+          </Text>
+          <Text size="sm">{author.followers} followers</Text>
         </div>
+        {[...Array(stars)].map((i) => (
+          <Star key={i} color="gold" fill="gold" />
+        ))}
       </Group>
       <Text className={classes.body} size="lg">
         {body}
@@ -45,15 +69,19 @@ function Testimonial({ body, author }: CommentSimpleProps) {
 }
 
 export default function TestimoniesComponent() {
+  const autoplay = useRef(Autoplay({ delay: 10000 }));
+
   return (
     <Carousel
       style={{
-        marginBottom: "2rem",
+        marginBottom: "1rem",
         maxWidth: 620,
       }}
       withIndicators
-      height={200}
       loop
+      withControls={false}
+      height={300}
+      plugins={[autoplay.current]}
       mx="auto"
       align="center"
     >
