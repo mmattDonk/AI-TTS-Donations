3d753d2ee862eeb3ae2ad126b2691c47476594d5
mmattbtw <matt@mmatt.net>
2022-10-26 18:12:46 -0500

    Revert "xdd?"
    This reverts commit 14221b436f2bbbdc8aa8995c572cc398147673f3.

--- apps/frontend/src/pages/api/streamers/streamerId/[streamerId].ts
+++ apps/frontend/src/pages/api/streamers/streamerId/[streamerId].ts
@@ -1,20 +1,17 @@
-import { edgePrisma as prisma } from '@solrock/prisma';
-// eslint-disable-next-line @next/next/no-server-import-in-page
-import { NextRequest, NextResponse } from 'next/server';
+import { prisma } from '@solrock/prisma';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export const config = {
-	runtime: 'experimental-edge',
-};
-
-export default async function handler(req: NextRequest, res: NextResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 	// if secret not in request headers, return unauthorized
-	if (!req.headers.get('secret')) {
-		return NextResponse.json({ error: 'Unauthorized', status: 403 }, { status: 403 });
-	} else if (req.headers.get('secret') !== process.env.API_SECRET) {
-		return NextResponse.json({ error: 'Unauthorized', status: 403 }, { status: 403 });
+	if (!req.headers.secret) {
+		res.status(401).json({ message: 'Unauthorized' });
+		return;
+	} else if (req.headers.secret !== process.env.API_SECRET) {
+		res.status(403).json({ message: 'Unauthorized' });
+		return;
 	}
 
-	const streamerId = req.nextUrl.searchParams.get('streamerId');
+	const { streamerId } = req.query;
 	console.debug(streamerId);
 	const streamer = await prisma.streamer.findFirst({
 		where: {
@@ -34,7 +31,8 @@ export default async function handler(req: NextRequest, res: NextResponse) {
 	});
 
 	if (!streamer) {
-		return NextResponse.json({ error: 'Streamer not found', status: 404 }, { status: 404 });
+		res.status(404).json({ message: 'Streamer not found' });
+		return;
 	}
 
 	switch (req.method) {
@@ -42,6 +40,10 @@ export default async function handler(req: NextRequest, res: NextResponse) {
 		case 'GET':
 			console.debug(streamerId);
 			console.debug(streamer);
-			return NextResponse.json({ message: 'streamer found!', streamer: streamer, status: 200 }, { status: 200 });
+			res.status(200).json({
+				message: 'streamer found!',
+				streamer: streamer,
+			});
+			return;
 	}
 }
