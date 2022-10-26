e7f3d19b3f89b6d9fac6dda86b4aefeed58dae54
mmattbtw <matt@mmatt.net>
2022-10-26 17:12:49 -0500

    better

--- apps/frontend/src/components/BetaBanner.tsx
+++ apps/frontend/src/components/BetaBanner.tsx
@@ -1,10 +1,14 @@
-import { Badge } from '@mantine/core';
+import { Badge, Stack } from '@mantine/core';
 
 export default function BetaBanner() {
 	return (
-		<Badge style={{ marginTop: '1rem' }} color="yellow" variant="outline">
-			This is a BETA website. Please report bugs to the <a href="https://discord.gg/VUAjRrkZVJ">mmattDonk Discord server</a>.
-			(only thing not working at the moment are blacklisted things and fallback voices, however that config will still save if you put stuff in there, so when those things do get working it will instantly apply with your config)
-		</Badge>
+		<Stack>
+			<Badge style={{ marginTop: '1rem' }} color="yellow" variant="outline">
+				This is a BETA website. Please report bugs to the <a href="https://discord.gg/VUAjRrkZVJ">mmattDonk Discord server</a>.
+			</Badge>
+			<Badge color="red" variant="outline">
+				Blacklisted (users, voices, and words) don't work, and fallback voices don't work.
+			</Badge>
+		</Stack>
 	);
 }
