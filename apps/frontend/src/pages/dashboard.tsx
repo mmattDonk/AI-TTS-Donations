0a5d12618e90c14336967dfe1dd42af3c9c6eeaa
mmattbtw <matt@mmatt.net>
2023-01-07 18:43:00 -0600

    uberduck fallback disclaimer

--- apps/frontend/src/pages/dashboard.tsx
+++ apps/frontend/src/pages/dashboard.tsx
@@ -334,14 +334,16 @@ export default function Dashboard() {
 											onBlur={async () => await saveConfig()}
 										/>
 										<h3>{t('Dashboard.configuration.fallbacksHeading')}</h3>
-										<TextInput
-											value={config.fallbackVoice ?? ''}
-											onChange={(event) => {
-												setConfig({ ...config, fallbackVoice: event.target.value ?? '' });
-											}}
-											label={t('Dashboard.configuration.fallbacksVoiceLabel')}
-											onBlur={async () => await saveConfig()}
-										/>
+										<Tooltip label={t('Dashboard.configuration.fallbacksInputTooltip')}>
+											<TextInput
+												value={config.fallbackVoice ?? ''}
+												onChange={(event) => {
+													setConfig({ ...config, fallbackVoice: event.target.value ?? '' });
+												}}
+												label={t('Dashboard.configuration.fallbacksVoiceLabel')}
+												onBlur={async () => await saveConfig()}
+											/>
+										</Tooltip>
 									</>
 								)}
 
