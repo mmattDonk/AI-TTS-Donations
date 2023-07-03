2f29eb19d03fc945bf593af409e874099f01703f
mmattbtw <matt@mmatt.net>
2023-07-02 19:01:31 -0500

    cache overlayid too

--- apps/frontend/src/pages/api/streamers/overlayId/[overlayId].ts
+++ apps/frontend/src/pages/api/streamers/overlayId/[overlayId].ts
@@ -1,9 +1,20 @@
 // mmattDonk 2023
 // https://mmattDonk.com
 
+import { Redis } from '@upstash/redis';
+import https from 'https';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { env } from '../../../../utils/env';
 import prismaClient from '../../../../utils/prisma';
+
+const redis = new Redis({
+	url: process.env.REDIS_URL ?? '',
+	token: process.env.REDIS_TOKEN ?? '',
+	agent: new https.Agent({
+		keepAlive: true,
+	}),
+});
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 	// if secret not in request headers, return unauthorized
 	if (!req.headers.secret) {
@@ -15,6 +26,19 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 	}
 
 	const { overlayId } = req.query;
+
+	if (req.method === 'GET') {
+		const redisCache = await redis.get(overlayId as string);
+
+		if (redisCache) {
+			res.status(200).json({
+				message: 'streamer found!',
+				streamer: redisCache,
+			});
+			return;
+		}
+	}
+
 	console.debug(overlayId);
 	const streamer = await prismaClient.streamer.findFirst({
 		where: {
@@ -34,7 +58,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
 	// filter out the email field from the user object
 	streamer.user.email = null;
-
+	if (req.method === 'GET') {
+		await redis.set(overlayId as string, JSON.stringify(streamer), {
+			ex: 60 * 5,
+		});
+	}
 	switch (req.method) {
 		case 'GET':
 			console.debug(overlayId);
