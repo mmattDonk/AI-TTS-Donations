4c5ebcb98c1a32b749c43f927aae8dc303b9dc0f
mmattbtw <matt@mmatt.net>
2023-08-02 19:50:46 -0500

    Shutdown Announcement

--- apps/frontend/src/components/BetaBanner.tsx
+++ apps/frontend/src/components/BetaBanner.tsx
@@ -5,11 +5,16 @@ import { Badge, Stack } from '@mantine/core';
 import { useTranslations } from 'next-intl';
 
 export default function BetaBanner() {
-	const t = useTranslations('beta');
+	const t = useTranslations();
 	return (
 		<Stack>
 			<Badge style={{ marginTop: '1rem' }} color="yellow" variant="outline">
-				{t.rich('header', {
+				{t.rich('beta.header', {
+					discordLink: (children) => <a href="https://discord.gg/VUAjRrkZVJ">{children}</a>,
+				})}
+			</Badge>
+			<Badge color="red">
+				{t.rich('shutdown', {
 					discordLink: (children) => <a href="https://discord.gg/VUAjRrkZVJ">{children}</a>,
 				})}
 			</Badge>
