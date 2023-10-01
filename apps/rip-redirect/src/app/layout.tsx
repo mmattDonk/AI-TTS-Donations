d097fe32dae37280ac418c95ec2181b1e5db4d63
mmattbtw <matt@mmatt.net>
2023-10-01 00:06:31 -0500

    fix ig

--- apps/rip-redirect/src/app/layout.tsx
+++ apps/rip-redirect/src/app/layout.tsx
@@ -1,10 +1,6 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
-
-export const metadata: Metadata = {
+export const metadata = {
 	title: 'Solrock has shutdown.',
 	description: 'Checkout TTS.monster.',
 };
@@ -12,7 +8,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body>{children}</body>
 		</html>
 	);
 }
